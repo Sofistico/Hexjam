80114b386041315e2ee3999c819bf84bd7dd78d7
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-01-09 17:24:58 -0300

    Adding properly the hunger effect

--- Common/src/main/java/net/hexjam/registry/HexJamPatternRegistry.java
+++ Common/src/main/java/net/hexjam/registry/HexJamPatternRegistry.java
@@ -1,13 +1,18 @@
 package net.hexjam.registry;
 
 import at.petrak.hexcasting.api.PatternRegistry;
+import at.petrak.hexcasting.api.misc.MediaConstants;
 import at.petrak.hexcasting.api.spell.Action;
 import at.petrak.hexcasting.api.spell.math.HexDir;
 import at.petrak.hexcasting.api.spell.math.HexPattern;
+import at.petrak.hexcasting.api.utils.MediaHelper;
+import at.petrak.hexcasting.common.casting.operators.spells.OpPotionEffect;
 import kotlin.Triple;
 import net.hexjam.casting.patterns.math.OpSignum;
 import net.hexjam.casting.patterns.spells.OpCongrats;
 import net.hexjam.casting.patterns.spells.OpShears;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Identifier;
 import net.hexjam.casting.*;
 
@@ -23,7 +28,7 @@ public class HexJamPatternRegistry {
     public static HexPattern CONGRATS = registerPerWorld(HexPattern.fromAngles("eed", HexDir.WEST), "congrats", new OpCongrats());
     public static HexPattern SIGNUM = register(HexPattern.fromAngles("edd", HexDir.NORTH_WEST), "signum", new OpSignum());
     public static HexPattern SHEARS = register(HexPattern.fromAngles("eed", HexDir.WEST), "shears", new OpShears());
-    public static HexPattern HUNGER = register(HexPattern.fromAngles("aqqqqqeawaw", HexDir.NORTH_WEST), "potion/hunger", new OpShears());
+    public static HexPattern HUNGER = register(HexPattern.fromAngles("aqqqqqeawaw", HexDir.NORTH_WEST), "potion/hunger", new OpPotionEffect(StatusEffects.HUNGER, MediaConstants.DUST_UNIT, true, false, false));
 
     public static void init() {
         try {
